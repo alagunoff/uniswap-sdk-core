b67627fe7be65bcbd706e6787cef2234ccd86d11
alagunoff <lagunov.artamon@gmail.com>
2025-02-04 12:08:13 +0300

    refactor: remove deprecated chain IDs and associated WETH9 tokens

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,6 @@ import JSBI from 'jsbi';
 export type BigintIsh = JSBI | string | number;
 
 export enum ChainId {
-  MAINNET = 1,
-  ROPSTEN = 3,
-  RINKEBY = 4,
-  GÖRLI = 5,
-  KOVAN = 42,
   POLYGON_AMOY = 80002,
 }
 
